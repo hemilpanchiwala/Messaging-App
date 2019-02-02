bdc0e7aa4937c766697ca315c4cfa78b2508dce9
hemilpanchiwala <hpanchiwala@cs.iitr.ac.in>
2019-02-02 22:51:39 +0530

    Internal Changes

--- app/src/main/java/com/example/hemant/fragment/MainActivity.java
+++ app/src/main/java/com/example/hemant/fragment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hemant.fragment;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_main);
+        
 
     }
 
